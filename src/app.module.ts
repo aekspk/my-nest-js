b02d8df508c5b0d3490ef62e81eb142e4cf0e04c
Aekachai.S <aekachai.s@ip-192-168-1-101.ap-southeast-7.compute.internal>
2026-04-11 12:02:01 +0700

    Add db seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
+import { CoreModule } from './core/core.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProductsModule],
+  imports: [ConfigModule.forRoot(), ProductsModule, CoreModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
